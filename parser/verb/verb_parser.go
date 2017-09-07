d23913e38fddd8cbbef2ee6d8bc9618e080b6494
Marcus Posey <marcusposey@gmail.com>
2017-09-07 13:04:15 -0500

    Decompose function and add documentation

--- parser/verb/verb_parser.go
+++ parser/verb/verb_parser.go
@@ -22,13 +22,13 @@ type VerbParser struct {
 	waitGroup sync.WaitGroup
 }
 
-// NewVerbParser creates a new *VerbParser that is connected to db and
-// uses threadCount threads.
+// NewVerbParser creates a *VerbParser that is connected to db and
+// uses threadCount workers.
 //
 // threadCount must be > 0.
 //
 // pageLimit indicates how many pages should be consumed. If set to -1,
-// Parse will always return a true value. When set to N, Consume will
+// Parse will always return a true value. When set to N, Parse will
 // begin returning false after it has been called N times.
 // Valid values are: {-1} U [1, INT_MAX]
 func NewVerbParser(db model.Database, threadCount,
@@ -36,42 +36,52 @@ func NewVerbParser(db model.Database, threadCount,
 	if threadCount < 1 {
 		return nil, errors.New("Thread count for VerbConsumer must be at least 1")
 	}
-	if pageLimit < -1 {
+	if pageLimit < -1 || pageLimit == 0 {
 		return nil, errors.New("pageLimit must be in {-1} U [1, INT_MAX]")
 	}
 
-	// Pulled this bad boy out of a hat. Remember: it's not magic
-	// if you give it a name ;D
-	const queueSize = 5000
+	// TODO: Find optimal job queue size.
+	const jobQueueSize = 5000
 
 	vparser := &VerbParser{
 		PageLimit:     pageLimit,
 		PagesConsumed: 0,
-		jobQueue:      make(chan parser.Page, queueSize),
+		jobQueue:      make(chan parser.Page, jobQueueSize),
 	}
+	vparser.spawnWorkers(threadCount, db)
 
-	vparser.waitGroup.Add(threadCount)
+	return vparser, nil
+}
 
-	for i := 0; i < threadCount; i++ {
+// spawnWorkers creates workerCount parallel workers that take jobs from the
+// job queue and store results in db.
+func (vparser *VerbParser) spawnWorkers(workerCount int, db model.Database) {
+	vparser.waitGroup.Add(workerCount)
+
+	for i := 0; i < workerCount; i++ {
 		go func(wkr worker, wg *sync.WaitGroup) {
 			defer wg.Done()
 			wkr.Start()
 		}(NewWorker(db, vparser.jobQueue), &vparser.waitGroup)
 	}
-	return vparser, nil
 }
 
 // Wait requests that all workers finish processing page content.
+// You should call this method before interacting with results produced
+// by this parser.
 func (vparser *VerbParser) Wait() {
 	close(vparser.jobQueue)
 	vparser.waitGroup.Wait()
 }
 
-// Parse conditionally adds the contents of page to a database.
+// Parse searches page for verbs and adds their templates to a database.
 //
-// If any section of page contains a verb definition, that verb and its
-// metadata are inserted in the database defined by consumer.Key. Pages
-// that do not contain verb definitions are ignored.
+// These templates explain what form the verb is in (e.g., infinitive or
+// indicative) and how it can change depending on the context of its
+// usage (e.g., 1st person, singular, present tense).
+//
+// This is a mostly nonblocking call. You should invoke Wait to ensure
+// results ready.
 func (vparser *VerbParser) Parse(page parser.Page) (bool, error) {
 	if vparser.PagesConsumed >= vparser.PageLimit &&
 		vparser.PageLimit != -1 {
