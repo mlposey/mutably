dd6100cfaa905d9688c319f18bbf21af7b9dc15a
Marcus Posey <marcusposey@gmail.com>
2017-09-11 18:05:12 -0500

    Document components

--- anvil/model/component.go
+++ anvil/model/component.go
@@ -1,10 +1,21 @@
 package model
 
+// Language is the description of a natural language.
+// This includes things like English, Dutch, and German.
 type Language string
 
+// Verb defines a word that is a verb in a specific language.
 type Verb struct {
+	// The language of the verb
 	Language Language
-	Text     string
+	// The actual verb string
+	Text string
 }
 
+// VerbTemplate describes how a verb presents itself in different contexts.
+//
+// Templates can be simple (e.g., {{nl-verb}}) or complex (e.g.,
+// '{{nl-verb form of|n=sg|t=past|m=subj|treden}}').
+// They most often describe some number of grammatical attributes that you
+// can read about here: https://en.wikipedia.org/wiki/Finite_verb#Grammatical_categories
 type VerbTemplate string
