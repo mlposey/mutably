3adfeb2240e6b006dab91edaa92b7dc4362145ef
Marcus Posey <marcusposey@gmail.com>
2017-09-12 13:37:28 -0500

    Create singleton for getting conjugators

--- anvil/model/inflection/conjugator.go
+++ anvil/model/inflection/conjugator.go
@@ -1,6 +1,9 @@
 package inflection
 
-import "mutably/anvil/model"
+import (
+	"mutably/anvil/model"
+	"sync"
+)
 
 // A Conjugator builds conjugation tables using verb templates.
 type Conjugator interface {
@@ -12,4 +15,39 @@ type Conjugator interface {
 	SetDatabase(model.Database) error
 	// Conjugate should build part (or all of) a conjugation table.
 	Conjugate(model.VerbTemplate) error
+}
+
+// Conjugators is a singleton that uses the description of a language
+// return its conjugator.
+// This object can be retrieved by calling GetConjugators().
+type Conjugators struct {
+	c map[string]Conjugator
+}
+
+func (conj *Conjugators) Get(language string) Conjugator {
+	return conj.c[language]
+}
+
+var (
+	once                sync.Once
+	conjugatorsInstance *Conjugators
+)
+
+// GetConjugators returns the single instance of Conjugators.
+func GetConjugators() *Conjugators {
+	once.Do(func() {
+		conjugatorsInstance = &Conjugators{make(map[string]Conjugator)}
+
+		items := []Conjugator{
+			// Put new Conjugator implementations here.
+			&Dutch{},
+			// ----------------------------------------
+		}
+		for _, conjugator := range items {
+			for _, language := range conjugator.GetLanguages() {
+				conjugatorsInstance.c[string(language)] = conjugator
+			}
+		}
+	})
+	return conjugatorsInstance
 }
