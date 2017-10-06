f526182664263a5824ffc95b5f77b65ea94d22cb
Marcus <marcusposey@gmail.com>
2017-10-06 02:54:56 -0500

    Add missing character to lbzip2 command

--- archive/get-archive.sh
+++ archive/get-archive.sh
@@ -2,4 +2,4 @@
 # The docker-compose file requires a pages dump in this directory.
 # This script should be run before bringing up the containers.
 wget https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-meta-current.xml.bz2
-lbzip2 -d -k enwiktionary-latest-pages-meta-current.xml.bz
+lbzip2 -d -k enwiktionary-latest-pages-meta-current.xml.bz2
